39efdb2b59db789a84c798851d66ddb369cf82c3
maheshsnn <maheshsnn@gmail.com>
2018-10-30 02:21:25 +0530

    latest

--- Practice/facebook.bat
+++
@@ -1,5 +0,0 @@
-cd C:\Users\mahesh\Documents\SELENIUM WORKSPACE\Practice
-
-Set classpath=C:\Users\mahesh\Documents\SELENIUM WORKSPACE\Practice\bin;C:\Users\mahesh\Documents\SELENIUM WORKSPACE\lib\*;
-
-java org.testng.TestNG testng.xml
